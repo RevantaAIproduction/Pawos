01f7d05880a20f1ceec5888880fb276662b0f923
Tharun Esta <tharun.esta@gmail.com>
2026-09-14 20:32:14 +0530

    assets

--- pawos-web/src/components/GlobalBackground.tsx
+++ pawos-web/src/components/GlobalBackground.tsx
@@ -1,1 +1,9 @@
-﻿
+"use client";
+import { usePathname } from "next/navigation";
+import { HeroAnimation } from "./HeroAnimation";
+
+export function GlobalBackground() {
+  const pathname = usePathname();
+  if (pathname?.startsWith("/docs")) return null;
+  return <HeroAnimation />;
+}
